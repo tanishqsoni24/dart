df0af8add2a92f978ac1e7433b7d0fa9c4e8cb9e
tanishqsoni0309 <tanishqsoni0309@gmail.com>
2023-08-15 20:20:17 +0530

    initialising-with-some-content

---
+++ playground.dart
@@ -0,0 +1,9 @@
+// dart:core package is imported by default
+// you may also import packages like : import 'dart:io';
+
+main(){
+    print("hello world");
+    var firstName = 'Tanishq';
+    String lastName = 'Soni';
+    print(firstName + " " + lastName);    
+}   
